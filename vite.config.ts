69bd8477738bbd8bfaf18bd0a866869044e389b7
Phan Hien <phanhien.tin@gmail.com>
2025-11-11 12:06:11 +0700

    Clean repo without node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ← quan trọng: đảm bảo các file assets được tham chiếu đúng
   plugins: [react()],
   css: {
     postcss: {
